8176f244b4d16b3511fd27f2841fff98ab9b6377
cessdel <cesardelgadocardona@gmail.com>
2024-09-30 10:53:36 -0600

    A

--- modelo.py
+++ modelo.py
@@ -39,63 +39,6 @@ class ReggressionModel:
         logging.info(f"Model saved to {self.model_path}")
 
 
-    def train_model(self):
-        #config_path = 'config.json'
-        # # Cargar datos desde el archivo JSON de configuración
-        # with open(config_path, 'r') as f:
-        #     config = json.load(f)
-        # DB_PORT = config.get('influx_port')
-        # DB_HOST = config.get('influx_host')
-        # DB_BUCKET = config.get('influx_bucket')
-        DB_PORT = "8086"
-        DB_HOST = '192.168.100.79'
-        DB_ORG = 'SmartHome'
-        DB_TOKEN = '_VmYvh5wFRpx8EO7ntZpuEW-o0h3bWHnsBC_r5UkLi3hsZuHNkCTAhogWF-fkcpNRcT96SpDJw-Ek0cVa9ce6A=='
-        DB_BUCKET = 'SmartHomeData'
-        url=f'http://{str(DB_HOST)}:{str(DB_PORT)}'
-        logging.info("Training started.")
-        print("Training started.")
-        self.training_status = "In progress"
-        try:
-            query_api = self.client.query_api()
-            query = f"""
-            from(bucket: "{DB_BUCKET}")
-                |> range(start: -100d)  // Ajusta el rango para el entrenamiento
-                |> filter(fn: (r) => r["_measurement"] == "temperatura")
-                |> keep(columns: ["_time", "_value"])
-            """
-            tables = query_api.query(query)
-
-            data = []
-            times = []
-
-            for table in tables:
-                for record in table.records:
-                    times.append(record.get_time())
-                    data.append(record.get_value())
-
-            df = pd.DataFrame({'time': times, 'temperature': data})
-            df['timestamp'] = df['time'].astype(np.int64) // 10**9  # Convertir a segundos desde epoch
-
-            X = df['timestamp'].values.reshape(-1, 1)
-            y = df['temperature'].values
-
-            self.model.fit(X, y)
-            self.save_model()
-
-            y_pred_train = self.model.predict(X)
-            mse = mean_squared_error(y, y_pred_train)
-            mae = mean_absolute_error(y, y_pred_train)
-            r2 = r2_score(y, y_pred_train)
-
-            logging.info(f"Training completed.\nMean Squared Error (MSE): {mse}\nMean Absolute Error (MAE): {mae}\nR²: {r2}")
-            self.training_status = "Completed"
-
-        except Exception as e:
-            logging.error("Error during training: %s", e)
-            print(f"Error during training: {e}")
-            self.training_status = "Failed"
-
     def predict_future_temperatures(self, hours=12):
         now = datetime.now()  # Utiliza UTC
         print(f"Current time: {now}")
